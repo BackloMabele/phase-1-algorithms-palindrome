76a92d47b506ba03d8a7c8266e66b0cabab7e8a7
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-10-03 23:56:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let lowerCaseWord = word.toLowerCase()
+  let reversedWord = lowerCaseWord.split('').reverse().join('');
+  return lowerCaseWord === reversedWord
 }
 
 /* 
-  Add your pseudocode here
+  write a function called isPalindome
+  change the word or statement to lowercase
+  split the word to obtain all the letters in an array
+  next is to reverse the order of the letters in a new array
+  next is to join the elements in a string
+  finally, to determine whether the word is a palindrone or not
 */
 
 /*
-  Add written explanation of your solution here
+  my solution is a function, isPalindrone that takes a string as an argument
+  in the function, the string is converted to lowercase
+  the lowercase string is held in a variable, lowerCaseWord
+  the string is then converted to an array of letters using split(' ')
+  the array of letters is then reversed using reverse()
+  the reversed array of letters is then converted back to a string using join ()
+  the reversed string is stored in the variable lowerCaseWord
+  finally, a comparison of the two strings, lowerCaseWord and reversedWord to determine true or false
 */
 
 // You can run `node index.js` to view these console logs
